f77d00c6f63789018243a22a10eed286460ae8e0
kyk9400 <kyk9400@nate.com>
2021-01-18 19:16:23 +0900

    git init

---
+++ src/main/java/kh/cocoa/CocoaApplication.java
@@ -0,0 +1,13 @@
+package kh.cocoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CocoaApplication.class, args);
+    }
+
+}
