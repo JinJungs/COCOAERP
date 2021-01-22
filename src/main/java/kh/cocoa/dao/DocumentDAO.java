3f876d2e1783ac9dc2e9488c851f4aa410df8abb
Hyokyung <twomoon2411@naver.com>
2021-01-22 16:07:17 +0900

    검색기능 완료

--- src/main/java/kh/cocoa/dao/DocumentDAO.java
+++ src/main/java/kh/cocoa/dao/DocumentDAO.java
@@ -3,7 +3,6 @@ package kh.cocoa.dao;
 import java.sql.Date;
 import java.util.List;
 
-import kh.cocoa.dto.TemplatesDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import kh.cocoa.dto.DocumentDTO;
@@ -11,14 +10,15 @@ import kh.cocoa.dto.DocumentDTO;
 @Mapper
 public interface DocumentDAO {
 	//임시저장한 문서 리스트
-	public List<DocumentDTO> getTemporaryList();
-	public List<DocumentDTO> getSearchTemporaryList(Date startDate, Date endDate, String template);
+	public List<DocumentDTO> getSearchTemporaryList(String empCode, Date startDate, Date endDate, List<String> templateList, String searchOption, String searchText);
 	//상신한 문서 리스트
-	public List<DocumentDTO> getRaiseList();
+	public List<DocumentDTO> getSearchRaiseList(String empCode, Date startDate, Date endDate, List<String> templateList, String searchOption, String searchText);
+	
 	//승인된 문서 리스트
-	public List<DocumentDTO> getApprovalList();
+	public List<DocumentDTO> getSearchApprovalList(String empCode, Date startDate, Date endDate, List<String> templateList, String searchOption, String searchText);
+	
 	//반려된 문서 리스트
-	public List<DocumentDTO> getRejectList();
+	public List<DocumentDTO> getSearchRejectList(String empCode, Date startDate, Date endDate, List<String> templateList, String searchOption, String searchText);
 	//회수한 문서 리스트
-	public List<DocumentDTO> getReturnList();
-}
+	public List<DocumentDTO> getSearchReturnList(String empCode, Date startDate, Date endDate, List<String> templateList, String searchOption, String searchText);
+	}
--- src/main/java/kh/cocoa/dto/EmployeeDTO.java
+++ src/main/java/kh/cocoa/dto/EmployeeDTO.java
@@ -12,6 +12,7 @@ public class EmployeeDTO {
     private int code;
     private String name;
     private String password;
+    private String phone;
     private String office_phone;
     private String address;
     private String email;
@@ -19,22 +20,34 @@ public class EmployeeDTO {
     private String gender;
     private Date hire_date;
     private String withdraw;
+    private int dept_code;
+    private int pos_code;
+    private int team_code;
+    
+    //추가부분
     private String deptname;
     private String teamname;
-
+    
     @Builder
-    public EmployeeDTO(int code, String name, String password, String office_phone, String address, String email, String b_email, String gender, Date hire_date, String withdraw, String deptname, String teamname) {
-        this.code = code;
-        this.name = name;
-        this.password = password;
-        this.office_phone = office_phone;
-        this.address = address;
-        this.email = email;
-        this.b_email = b_email;
-        this.gender = gender;
-        this.hire_date = hire_date;
-        this.withdraw = withdraw;
-        this.deptname = deptname;
-        this.teamname = teamname;
-    }
+   public EmployeeDTO(int code, String name, String password, String phone, String office_phone, String address,
+         String email, String b_email, String gender, Date hire_date, String withdraw, int dept_code, int pos_code,
+         int team_code, String deptname, String teamname) {
+      super();
+      this.code = code;
+      this.name = name;
+      this.password = password;
+      this.phone = phone;
+      this.office_phone = office_phone;
+      this.address = address;
+      this.email = email;
+      this.b_email = b_email;
+      this.gender = gender;
+      this.hire_date = hire_date;
+      this.withdraw = withdraw;
+      this.dept_code = dept_code;
+      this.pos_code = pos_code;
+      this.team_code = team_code;
+      this.deptname = deptname;
+      this.teamname = teamname;
+   }    
 }
