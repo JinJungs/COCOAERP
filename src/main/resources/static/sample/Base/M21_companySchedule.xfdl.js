6e70466a833ae1243c1106fe812955d5a162f83d
smoothlife <test@test.com>
2021-02-25 17:39:55 +0900

    v3

--- src/main/resources/static/sample/Base/M21_companySchedule.xfdl.js
+++ src/main/resources/static/sample/Base/M21_companySchedule.xfdl.js
@@ -23,60 +23,66 @@
             
             // UI Components Initialize
             obj = new Static("Static00","20","10",null,"34","10",null,null,null,null,null,this);
-            obj.set_taborder("0");
+            obj.set_taborder("2");
             obj.set_text("회사 일정 관리");
             obj.set_cssclass("sta_WF_title01");
-            obj.set_font("bold 18px/normal \"Malgun Gothic\"");
+            obj.set_font("bold 18px/normal \"Malgun gothic\",\"Arial\",\"Gulim\"");
             this.addChild(obj.name, obj);
 
-            obj = new Div("divSearch","20","100","1000","50",null,null,null,null,null,null,this);
-            obj.set_taborder("1");
+            obj = new Div("divSearch","20","100",null,"50","100",null,"905",null,null,null,this);
+            obj.set_taborder("2");
             obj.set_border("1px solid #c7c7c7");
             obj.set_text("");
             obj.set_background("#eeeeee");
             this.addChild(obj.name, obj);
 
-            obj = new Calendar("CldEnd","296","12","150","25",null,null,null,null,null,null,this.divSearch.form);
+            obj = new Calendar("CldEnd","276","12","150","25",null,null,null,null,null,null,this.divSearch.form);
             obj.set_taborder("0");
             this.divSearch.addChild(obj.name, obj);
 
-            obj = new Static("Static01","12","12","95","25",null,null,null,null,null,null,this.divSearch.form);
+            obj = new Static("Static01","10","12","50","25",null,null,null,null,null,null,this.divSearch.form);
             obj.set_taborder("1");
             obj.set_text("기간 선택");
             this.divSearch.addChild(obj.name, obj);
 
-            obj = new Static("Static02","276","12","10","25",null,null,null,null,null,null,this.divSearch.form);
+            obj = new Static("Static02","256","12","10","25",null,null,null,null,null,null,this.divSearch.form);
             obj.set_taborder("2");
             obj.set_text("-");
             this.divSearch.addChild(obj.name, obj);
 
-            obj = new Calendar("CldStart","112","12","150","25",null,null,null,null,null,null,this.divSearch.form);
+            obj = new Calendar("CldStart","92","12","150","25",null,null,null,null,null,null,this.divSearch.form);
             obj.set_taborder("3");
             this.divSearch.addChild(obj.name, obj);
 
-            obj = new Button("btnFind","473","12","54","25",null,null,null,null,null,null,this.divSearch.form);
+            obj = new Button("btnFind","453","12","54","25",null,null,null,null,null,null,this.divSearch.form);
             obj.set_taborder("4");
             obj.set_text("조회");
             obj.set_cssclass("btn_WF_search01");
             this.divSearch.addChild(obj.name, obj);
 
-            obj = new Button("btnAdd","849","12","60","25",null,null,null,null,null,null,this.divSearch.form);
+            obj = new Button("btnAdd",null,"12","70","25","80",null,null,null,null,null,this.divSearch.form);
             obj.set_taborder("5");
             obj.set_text("추가");
             obj.set_cssclass("btn_WF_add01");
             this.divSearch.addChild(obj.name, obj);
 
-            obj = new Button("btnDel","919","12","60","25",null,null,null,null,null,null,this.divSearch.form);
+            obj = new Button("btnDel",null,"12","60","25","10",null,null,null,null,null,this.divSearch.form);
             obj.set_taborder("6");
             obj.set_text("삭제");
+            obj.set_cssclass("btn_WF_delete01");
             this.divSearch.addChild(obj.name, obj);
 
-            obj = new Grid("GridList","20","170","1055","460",null,null,null,null,null,null,this);
-            obj.set_taborder("2");
+            obj = new Div("Div00","20","170",null,null,"100","50","905",null,null,null,this);
+            obj.set_taborder("0");
+            obj.set_text("Div00");
+            this.addChild(obj.name, obj);
+
+            obj = new Grid("GridList","0.00%","0",null,null,"0.00%","0.00%",null,null,null,null,this.Div00.form);
+            obj.set_taborder("0");
             obj.set_binddataset("dsSchedule");
             obj.set_autofittype("col");
-            obj._setContents("<Formats><Format id=\"default\"><Columns><Column size=\"80\"/><Column size=\"80\"/><Column size=\"80\"/><Column size=\"80\"/><Column size=\"80\"/><Column size=\"80\"/><Column size=\"80\"/><Column size=\"80\"/><Column size=\"80\"/></Columns><Rows><Row size=\"24\" band=\"head\"/><Row size=\"24\"/></Rows><Band id=\"head\"><Cell text=\"번호\"/><Cell col=\"1\" text=\"제목\"/><Cell col=\"2\" text=\"내용\"/><Cell col=\"3\" text=\"시작기간\"/><Cell col=\"4\" text=\"종료기간\"/><Cell col=\"5\" text=\"부서코드\"/><Cell col=\"6\" text=\"팀코드\"/><Cell col=\"7\" text=\"사번\"/><Cell col=\"8\" text=\"작성자\"/></Band><Band id=\"body\"><Cell text=\"bind:seq\"/><Cell col=\"1\" text=\"bind:title\"/><Cell col=\"2\" text=\"bind:contents\"/><Cell col=\"3\" text=\"bind:str_start_time\"/><Cell col=\"4\" text=\"bind:str_end_time\"/><Cell col=\"5\" text=\"bind:dept_code\"/><Cell col=\"6\" text=\"bind:team_code\"/><Cell col=\"7\" text=\"bind:emp_code\"/><Cell col=\"8\" text=\"bind:writer\"/></Band></Format></Formats>");
-            this.addChild(obj.name, obj);
+            obj._setContents("<Formats><Format id=\"default\"><Columns><Column size=\"25\"/><Column size=\"80\"/><Column size=\"80\"/><Column size=\"80\"/><Column size=\"80\"/><Column size=\"80\"/><Column size=\"80\"/><Column size=\"80\"/><Column size=\"80\"/></Columns><Rows><Row size=\"24\" band=\"head\"/><Row size=\"24\"/></Rows><Band id=\"head\"><Cell text=\"번호\"/><Cell col=\"1\" text=\"제목\"/><Cell col=\"2\" text=\"내용\"/><Cell col=\"3\" text=\"시작기간\"/><Cell col=\"4\" text=\"종료기간\"/><Cell col=\"5\" text=\"부서코드\"/><Cell col=\"6\" text=\"팀코드\"/><Cell col=\"7\" text=\"사번\"/><Cell col=\"8\" text=\"작성자\"/></Band><Band id=\"body\"><Cell text=\"bind:seq\" displaytype=\"text\"/><Cell col=\"1\" text=\"bind:title\"/><Cell col=\"2\" text=\"bind:contents\"/><Cell col=\"3\" text=\"bind:str_start_time\" edittype=\"none\"/><Cell col=\"4\" text=\"bind:str_end_time\" displaytype=\"normal\" edittype=\"none\"/><Cell col=\"5\" text=\"bind:dept_code\"/><Cell col=\"6\" text=\"bind:team_code\"/><Cell col=\"7\" text=\"bind:emp_code\"/><Cell col=\"8\" text=\"bind:writer\"/></Band></Format></Formats>");
+            this.Div00.addChild(obj.name, obj);
 
             // Layout Functions
             //-- Default Layout : this
@@ -119,12 +125,33 @@
         this.fn_callback = function(msg){
         	trace(msg);
         }
+        this.divSearch_btnFind_onclick = function(obj,e)
+        {
+        	var sch_start = this.divSearch.form.CldStart.value;
+        	var sch_end = this.divSearch.form.CldEnd.value;
+        	trace(sch_start);
+        	trace(sch_end);
+        };
+
+        this.divSearch_btnAdd_onclick = function(obj,e)
+        {
+
+        };
+
+        this.divSearch_btnDel_onclick = function(obj,e)
+        {
+
+        };
+
         });
         
         // Regist UI Components Event
         this.on_initEvent = function()
         {
             this.addEventHandler("onload",this.M21_companySchedule_onload,this);
+            this.divSearch.form.btnFind.addEventHandler("onclick",this.divSearch_btnFind_onclick,this);
+            this.divSearch.form.btnAdd.addEventHandler("onclick",this.divSearch_btnAdd_onclick,this);
+            this.divSearch.form.btnDel.addEventHandler("onclick",this.divSearch_btnDel_onclick,this);
         };
 
         this.loadIncludeScript("M21_companySchedule.xfdl");
