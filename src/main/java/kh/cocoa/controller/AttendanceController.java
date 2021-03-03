27c900af71c47765cf8c3870c7f35abf25267458
me <you@you.com>
2021-03-03 16:04:49 +0900

    채팅 - commit before push

--- src/main/java/kh/cocoa/controller/AttendanceController.java
+++ src/main/java/kh/cocoa/controller/AttendanceController.java
@@ -1,6 +1,5 @@
 package kh.cocoa.controller;
 
-import com.google.gson.JsonObject;
 import kh.cocoa.dto.AttendanceDTO;
 import kh.cocoa.dto.EmployeeDTO;
 import kh.cocoa.service.AttendanceService;
@@ -9,16 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 
 @Controller
